b76e8cb3e4798c8154502c192a03496b8c3c40d5
HansumaeCecilia <48033@edu.raseko.fi>
2023-05-09 11:01:57 +0300

    Lesson 5, activity 1

---
+++ minecraft_101_world_5/developer.py
@@ -0,0 +1,20 @@
+#*****************************************************  
+# First task of Python 101, world 5
+# developer.py 
+# @author: CK  
+# @since: Version date 09/05/2023  
+# @version: 1.0  
+# muutos: CK
+#*****************************************************/
+
+# Part 1
+agent.move(BACK, 1)
+agent.collect_all()
+agent.move(FORWARD, 7)
+agent.place(FORWARD)
+
+# Part 2
+agent.move(BACK, 7)
+agent.collect_all()
+agent.move(FORWARD, 7)
+agent.place(FORWARD)
